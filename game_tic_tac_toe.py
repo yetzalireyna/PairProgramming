0cf1139fc5c69464f9ccd2e42414d7675e2d15a6
yetzalireyna <77222032+yetzalireyna@users.noreply.github.com>
2021-07-27 16:18:42 -0600

    Create game_tic_tac_toe.py

---
+++ game_tic_tac_toe.py
@@ -0,0 +1,26 @@
+'''
+comments
+'''
+import TicTacToe as game
+import math
+
+def play(game, x_player, o_player, print_game=True):
+  if print_game:
+    game.print_board_nums()
+  letter = 'X'
+  
+  while game.empty_squares():
+    if letter == 'O':
+      square = o_player.get_move(game)
+    else:
+      square = x_player.get_move(game)
+    
+    if game.make_move(square, letter):
+      if print_game:
+        print(letter + f' makes a move to square {square}')
+        game.print_board()
+        print(' ') 
+        
+      letter = 'O' if letter == 'X' else 'X'
+      
+  
