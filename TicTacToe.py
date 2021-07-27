e5ec8094ca747407957621ebeab63f17da1eba6c
yetzalireyna <77222032+yetzalireyna@users.noreply.github.com>
2021-07-27 10:31:20 -0600

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,16 +9,17 @@ import random
 
 class TicTacToe:
     def __init__(self):
-        self.board = ['' for _ in range(9)]
-        self.current_winner = None
+        self.board = ['' for _ in range(9)]#create a board of nine squares
+        self.current_winner = None#keep track of the winner
         
     def print_board(self):
+        #get the rows
         for row in [self.board[i*3:(i*3] for i in range(3)]:
                     print('|' + '|'.join(row) + '|')
 
     @staticmethod
     def print_board_nums():
-
+#print what numbers are in each space
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('|' + '|'.join(row) + '|')
